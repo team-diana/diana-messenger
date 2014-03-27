477a761a1b76aa9cc728b93ecd83c1faa4ef5933
clynamen <clynamen@gmail.com>
2014-03-27 08:59:58 +0100

    Add cli arguments to test

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -8,14 +8,23 @@
 using namespace std;
 using namespace dianamessenger;
 
+int globalArgc;
+char** globalArgv;
+
+
 TEST(DianaMessenger, diagnostic)
 {
   ASSERT_EQ(1 + 1, 2);
+  Pantilt pantilt(globalArgc, globalArgv, "pantilt_test");
 }
 
 int main(int argc, char* argv[])
 {
   testing::InitGoogleTest(&argc, argv);
+
+  globalArgc = argc;
+  globalArgv = argv;
+
   return RUN_ALL_TESTS();
 }
 
